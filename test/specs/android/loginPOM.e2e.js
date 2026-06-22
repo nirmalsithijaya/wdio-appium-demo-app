44304964722389c2c9eb85b0bd348e0ef8908289
Nirmal Sithijaya Wewitavidana <nwewitavidana@pay.com.au>
2026-06-22 15:36:39 +0530

    updated POM

--- test/specs/android/loginPOM.e2e.js
+++ test/specs/android/loginPOM.e2e.js
@@ -19,7 +19,7 @@ describe("My Login Demo POM", () => {
 
   it("should login with valid credentials and logout", async () => {
     LoginScreen.login("bob@example.com", "10203040");
-    await expect(CatalogScreen.productsHeader).toHaveText("Products");
+    await expect(CatalogScreen.productsHeader).toHaveText("Items");
     await LeftSideMenuScreen.logout();
     await expect(LeftSideMenuScreen.logoutSuccessMessage).toHaveText("You are successfully logged out.");
   });
